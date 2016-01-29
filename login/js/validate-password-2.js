01e9ea74ab04740270e7d69d8cd2b61a1af8e490
Håkon von Hirsch <onkelh@gmail.com>
2016-01-29 08:46:12 +0100

    sdfgdsfg

--- login/js/validate-password-2.js
+++ login/js/validate-password-2.js
@@ -1,7 +1,7 @@
 function redirect(){
 	var password = document.getElementById("myInput");
 		if(myInput.value === "test") {
-			window.location.href = "http://www.dt.no";
+			window.location.href = "http://www.tb.no/tonsberg/nyheter/brann/seks-bedrifter-klager-pa-politiets-henleggelse/s/5-76-245765";
 		}
 		else window.location.href = "pw-attempt-2.html";
 }
