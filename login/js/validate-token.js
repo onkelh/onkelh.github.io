f7329cced4101f84c1e6cb52afe6efb8b8b7563e
Håkon von Hirsch <onkelh@gmail.com>
2016-01-29 08:42:02 +0100

    sadfsadf

--- login/js/validate-token.js
+++ login/js/validate-token.js
@@ -1,11 +1,22 @@
 function redirect(){
 	var password = document.getElementById("myInput");
-	var possibleTokens = ["HEST 89", "HEST89" "hest 89", "hest89"];
-	console.log(possibleTokens.indexOf(myInput.value) > -1);
 
 		if(myInput.value === "hest 89") {
 			window.location.href = "create-password.html";
 		}
+
+		if(myInput.value === "hest89") {
+			window.location.href = "create-password.html";
+		}
+
+		if(myInput.value === "HEST 89") {
+			window.location.href = "create-password.html";
+		}
+
+		if(myInput.value === "HEST89") {
+			window.location.href = "create-password.html";
+		}
+
 		else document.getElementById("vis-hidden").style.display = "block";
 		document.getElementById("message").style.display = "none";
 }
